157231647cfaecad0f1e0382b82a90cdc7ee4caa
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-17 22:05:04 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -9,66 +9,25 @@ class LandmarksController < ApplicationController
     erb :'landmarks/new'
   end
 
+
+###############################################
   post '/landmarks' do
-    #@figures = Figure.find_by(name: params[:figure][:name])
-    #if !params[:figure][:name].empty?
-    #  @figures << Figure.create(name: params[:figure][:name])
-    #end
-    #if params[:landmark][:landmark_ids] != nil
-    #  params[:landmark][:landmark_ids].each do |landmark|
-    #    id = landmark.gsub("landmark_","").to_i
-    #    @landmarks << Landmark.find(id)
-    #  end
-    #end
-    #@titles = []
-    #if !params[:title][:name].empty?
-    #  @titles << Title.create(params[:title])
-    #end
-    #if params[:figure][:title_ids] != nil
-    #  params[:figure][:title_ids].each do |title|
-    #    id = title.gsub("title_","").to_i
-    #    @titles << Title.find(id)
-    #  end
-    #end
-    #@figure = Figure.find_by(name: params[:figure][:name]) || Figure.create(name: "#{params[:figure][:name]}")
-    #@figure.landmarks = @landmarks
-    #@figure.titles = @titles
     redirect "/figures/#{@figure.id}"
   end
+###############################################
 
-  ##get '/landmarks/:id/edit' do
-    @figure = Figure.find("#{params[:id].to_i}")
-    erb :'figures/edit'
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find("#{params[:id].to_i}")
+    erb :'landmarks/edit'
   end
 
-  post '/figures/:id' do
-    @figure = Figure.find("#{params[:id]}")
-    @landmarks = []
-    if !params[:landmark][:name].empty?
-      @landmarks << Landmark.create(name: params[:landmark][:name])
-    end
-    if params[:figure][:landmark_ids] != nil
-      params[:figure][:landmark_ids].each do |landmark|
-        id = landmark.gsub("landmark_","").to_i
-        @landmarks << Landmark.find(id)
-      end
-    end
-    @titles = []
-    if !params[:title][:name].empty?
-      @titles << Title.create(params[:title])
-    end
-    if params[:figure][:title_ids] != nil
-      params[:figure][:title_ids].each do |title|
-        id = title.gsub("title_","").to_i
-        @titles << Title.find(id)
-      end
-    end
-    @figure.update(name: "#{params[:figure][:name]}")
-    @figure.landmarks = @landmarks
-    @figure.titles = @titles
-    @figure.save
-    redirect "/figures/#{@figure.id}"
+
+###############################################
+  post '/landmarks/:id' do
+    redirect "/landmarks/#{@landmark.id}"
   end
+###############################################
 
   get '/landmarks/:id' do
     @titles = Title.all
