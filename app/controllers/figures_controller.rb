44f291302c4c1e2128375a1b31a360d4066162c4
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-17 18:57:11 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,36 +9,25 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-
-    @landmarks = []
-    params[:landmark].each do |l|
-      landmark = Landmark.find_by(name: l.name) || Landmark.create(l)
-      @landmarks << landmark
+    if !params[:landmark][:name].empty?
+      @landmarks = Landmark.create(params[:landmark])
     end
-    if !params[:landmark_name].empty?
-      @landmarks << Landmark.create(name: params[:landmark_name], year: params[:landmark_year])
+    if !params[:title][:name].empty?
+      @titles = Title.create(params[:title])
     end
-
-    @titles = []
-    params[:title].each do |t|
-      title = Title.find_by(name: t.name) || Title.create(t)
-      @titles << title
-    end
-    if !params[:title_name].empty?
-      @titles << Title.create(name: params[:title_name])
-    end
-
-    @figure = Figure.create(name: params[:figure_name], titles: "#{@titles}", landmarks: "#{@landmarks}")
-
-    redirect "/figures/#{@figure.slug}"
+    @figure = Figure.create(params[:figure])
+    @figure.titles << @titles if @titles != nil
+    @figure.landmarks << @landmarks if @landmarks != nil
+    @figure.save
+    redirect "/figures/#{@figure.id}"
   end
 
-  get '/figures/:slug/edit' do
-    @figure = Figure.find_by_slug(params[:slug])
+  get '/figures/:id/edit' do
+    @figure = Figure.find("#{params[:id].to_i}")
     erb :'figures/edit'
   end
 
-  post '/figures/:slug' do
+  post '/figures/:id' do
 
     @landmarks = []
     params[:landmark].each do |l|
@@ -60,11 +49,13 @@ class FiguresController < ApplicationController
 
     @figure = Figure.update(name: params[:figure_name], titles: "#{@titles}", landmarks: "#{@landmarks}")
 
-    redirect "/figures/#{@figure.slug}"
+    redirect "/figures/#{@figure.id}"
   end
 
-  get '/figures/:slug' do
-    @slug = Figure.find_by_slug(params[:slug])
+  get '/figures/:id' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    @figure = Figure.find("#{params[:id].to_i}")
     erb :'figures/show'
   end
 
