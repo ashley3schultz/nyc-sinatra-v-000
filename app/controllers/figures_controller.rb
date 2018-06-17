7e2212ac04bfdb2d7156a14a56761a54cae533d8
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-17 19:08:13 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,39 +9,20 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-  @title = params[:title]
-  @title_ids = params[:figure][:title_ids]
-  @landmark = params[:landmark]
-  @landmark_ids = params[:figure][:landmark_ids]
-
-  @figure = Figure.create(:name => params[:figure][:name])
-  if !@title[:name].empty?
-    t = Title.create(:name => @title[:name])
-    @figure.titles << t
-  end
-  if @title_ids
-    @title_ids.each do |id|
-      t = Title.find(id)
-      @figure.titles << t
+    if !params[:landmark][:name].empty?
+      @landmarks = Landmark.create(params[:landmark])
     end
-  end
-  if !@landmark[:name].empty?
-    l = Landmark.create(:name => @landmark[:name])
-    @figure.landmarks << l
-  end
-  if @landmark_ids
-    @landmark_ids.each do |id|
-      l = Landmark.find(id)
-      @figure.landmarks << l
+    if !params[:title][:name].empty?
+      @titles = Title.create(params[:title])
     end
-  end
-  @figure.save
-  redirect to "/figures/#{@figure.id}"
+    @figure = Figure.create(params[:figure])
+    @figure.titles << @titles
+    @figure.landmarks << @landmarks
+    @figure.save
+    redirect "/figures/#{@figure.id}"
   end
 
   get '/figures/:id/edit' do
-    @titles = Title.all
-    @landmarks = Landmark.all
     @figure = Figure.find("#{params[:id].to_i}")
     erb :'figures/edit'
   end
