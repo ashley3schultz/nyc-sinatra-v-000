93ce6f0339750962dc442dae25e3c376481bd7e7
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-17 19:12:54 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,10 +10,10 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     if !params[:landmark][:name].empty?
-      @landmarks = Landmark.create(name: "#{params[:landmark][:name]}")
+      @landmarks = Landmark.create("#{params[:landmark][:name]}")
     end
     if !params[:title][:name].empty?
-      @titles = Title.create(params[:title])
+      @titles = Title.create("#{params[:title]}")
     end
     @figure = Figure.create(params[:figure])
     @figure.titles << @titles
